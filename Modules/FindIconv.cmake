0fcff71a00f26adae9e304ed1b65e445fb2d072d
Azat Khuzhin <a3at.mail@gmail.com>
2013-08-28 15:32:14 +0400

    [FindIconv] expand -t4

--- Modules/FindIconv.cmake
+++ Modules/FindIconv.cmake
@@ -2,24 +2,24 @@
 include(CheckSymbolExists)
 
 macro(FindIconv)
-	unset(LIBRARY CACHE)
-	find_library(LIBRARY NAMES iconv)
+    unset(LIBRARY CACHE)
+    find_library(LIBRARY NAMES iconv)
 
-	if (LIBRARY)
-		message(STATUS "Looking for iconv - found")
-		set(HAVE_ICONV_IN_LIBICONV TRUE)
-		set(LIBS ${LIBS} ${LIBRARY})
-	else (LIBRARY)
-	    #
-	    # Fix for iconv in libc
-	    #
-	    add_definitions(-DLIBICONV_PLUG)
+    if (LIBRARY)
+        message(STATUS "Looking for iconv - found")
+        set(HAVE_ICONV_IN_LIBICONV TRUE)
+        set(LIBS ${LIBS} ${LIBRARY})
+    else (LIBRARY)
+        #
+        # Fix for iconv in libc
+        #
+        add_definitions(-DLIBICONV_PLUG)
 
-		check_symbol_exists(iconv_open iconv.h HAVE_ICONV_IN_LIBC)
-		if (NOT HAVE_ICONV_IN_LIBC)
-			message(FATAL_ERROR "Can't find iconv in libc and libiconv")
-		else (NOT HAVE_ICONV_IN_LIBC)
-			message(STATUS "Found iconv in libc")
-		endif (NOT HAVE_ICONV_IN_LIBC)
-	endif (LIBRARY)
+        check_symbol_exists(iconv_open iconv.h HAVE_ICONV_IN_LIBC)
+        if (NOT HAVE_ICONV_IN_LIBC)
+            message(FATAL_ERROR "Can't find iconv in libc and libiconv")
+        else (NOT HAVE_ICONV_IN_LIBC)
+            message(STATUS "Found iconv in libc")
+        endif (NOT HAVE_ICONV_IN_LIBC)
+    endif (LIBRARY)
 endmacro(FindIconv)
