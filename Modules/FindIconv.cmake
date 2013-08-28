74e7fb14a9ee17dc4fd68b56a42169c26c9e9c60
Azat Khuzhin <a3at.mail@gmail.com>
2013-08-28 15:33:37 +0400

    [FindIconv] fix it for some of darwinds (that have installed iconv from ports)
    If you install libiconv from ports, than you will have next iconv
    headers:
    /usr/include/iconv.h
    /opt/local/include/iconv.h # for libiconv
    
    And it seems that the second one is used by gcc, and for this version we
    need LIBICONV_PLUG.
    
    Anyway adding LIBICONV_PLUG mustn't break anything for any other usual
    configuration

--- Modules/FindIconv.cmake
+++ Modules/FindIconv.cmake
@@ -5,16 +5,17 @@ macro(FindIconv)
     unset(LIBRARY CACHE)
     find_library(LIBRARY NAMES iconv)
 
+    #
+    # Fix for iconv in libc
+    # and some of darwins
+    #
+    add_definitions(-DLIBICONV_PLUG)
+
     if (LIBRARY)
         message(STATUS "Looking for iconv - found")
         set(HAVE_ICONV_IN_LIBICONV TRUE)
         set(LIBS ${LIBS} ${LIBRARY})
     else (LIBRARY)
-        #
-        # Fix for iconv in libc
-        #
-        add_definitions(-DLIBICONV_PLUG)
-
         check_symbol_exists(iconv_open iconv.h HAVE_ICONV_IN_LIBC)
         if (NOT HAVE_ICONV_IN_LIBC)
             message(FATAL_ERROR "Can't find iconv in libc and libiconv")
