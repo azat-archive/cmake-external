7234cb499baf46bd5ca147433d14daf86af51d3e
Azat Khuzhin <a3at.mail@gmail.com>
2014-12-06 18:51:22 +0300

    TargetCopy: target to copy files to dir

---
+++ Modules/TargetCopy.cmake
@@ -0,0 +1,14 @@
+# Simple target, that will just copy files to folder
+macro(TargetCopy target dir)
+    add_custom_target(${target} ALL COMMENT
+        "Copying ${target} files to the ${dir}"
+    )
+    add_custom_command(TARGET ${target} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${dir}
+    )
+    foreach(file ${ARGN})
+        add_custom_command(TARGET ${target} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy ${file} ${dir}
+        )
+    endforeach()
+endmacro(TargetCopy)
