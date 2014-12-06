7308364731afc3fb7988b22959116aeafb8bd42e
Azat Khuzhin <a3at.mail@gmail.com>
2014-12-06 18:30:09 +0300

    SetupSanitizers: macro to create targets with different sanitizers for clang;gcc

---
+++ Modules/SetupSanitizers.cmake
@@ -0,0 +1,30 @@
+include(AddCompilerFlags)
+
+# @args: compiler options for new targets
+#     example: -g3
+#
+macro(SetupSanitizers)
+    if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+        AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
+                         FLAGS ${ARGV} -fsanitize=thread
+                         OVERWRITE -fsanitize=thread
+                         LANGUAGES C CXX)
+        AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
+                         FLAGS "${ARGV} -fsanitize=address"
+                         OVERWRITE -fsanitize=address
+                         LANGUAGES C CXX)
+        AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEMEMORY
+                         FLAGS ${ARGV} -fsanitize=memory -fno-omit-frame-pointer
+                         OVERWRITE -fsanitize=memory -fsanitize-memory-track-origins
+                         LANGUAGES C CXX)
+    else()
+        AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
+                         FLAGS ${ARGV} "-fsanitize=thread -fPIC"
+                         OVERWRITE "-fsanitize=thread -pie"
+                         LANGUAGES C CXX)
+        AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
+                         FLAGS ${ARGV} -fsanitize=address
+                         OVERWRITE -fsanitize=address
+                         LANGUAGES C CXX)
+    endif()
+endmacro()
