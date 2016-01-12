b8a2751631f8865475317b7b183200390b72627a
Azat Khuzhin <a3at.mail@gmail.com>
2016-01-12 15:12:23 +0300

    SetupSanitizers: combine all FLAGS to one
    Otherwise it can fail since linker will have -fsanitize=XXX, while flags can be
    only -g3 (i.e. that passed via ARGV)

--- Modules/SetupSanitizers.cmake
+++ Modules/SetupSanitizers.cmake
@@ -6,37 +6,37 @@ include(AddCompilerFlags)
 macro(SetupSanitizers)
     if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
         AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
-                         FLAGS ${ARGV} -fsanitize=thread
+                         FLAGS "${ARGV} -fsanitize=thread"
                          OVERWRITE -fsanitize=thread
                          LANGUAGES C CXX)
         AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
-                         FLAGS ${ARGV} -fsanitize=address
+                         FLAGS "${ARGV} -fsanitize=address"
                          OVERWRITE -fsanitize=address
                          LANGUAGES C CXX)
         AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEMEMORY
-                         FLAGS ${ARGV} -fsanitize=memory -fno-omit-frame-pointer
+                         FLAGS "${ARGV} -fsanitize=memory -fno-omit-frame-pointer"
                          OVERWRITE -fsanitize=memory -fsanitize-memory-track-origins
                          LANGUAGES C CXX)
     else()
         if ((CMAKE_COMPILER_IS_GNUCC) AND
             (CMAKE_C_COMPILER_VERSION VERSION_GREATER "5.2"))
             AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
-                             FLAGS ${ARGV} -fsanitize=thread
+                             FLAGS "${ARGV} -fsanitize=thread"
                              OVERWRITE -fsanitize=thread
                              LANGUAGES C CXX)
             AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
-                             FLAGS ${ARGV} -fsanitize=address
+                             FLAGS "${ARGV} -fsanitize=address"
                              OVERWRITE -fsanitize=address
                              LANGUAGES C CXX)
         else()
             AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
-                             FLAGS ${ARGV} "-fsanitize=thread -fPIC"
+                             FLAGS "${ARGV} -fsanitize=thread -fPIC"
                              OVERWRITE "-fsanitize=thread -pie"
                              LANGUAGES C CXX)
             set(CMAKE_EXE_LINKER_FLAGS_SANITIZETHREAD "-pie")
             set(CMAKE_SHARED_LINKER_FLAGS_SANITIZETHREAD "-pie")
             AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
-                             FLAGS ${ARGV} -fsanitize=address
+                             FLAGS "${ARGV} -fsanitize=address"
                              OVERWRITE -fsanitize=address
                              LANGUAGES C CXX)
         endif()
