7118372bb52e0e7c11050a4b0176001d8d1a6542
Azat Khuzhin <a3at.mail@gmail.com>
2016-01-12 13:58:46 +0300

    SetupSanitizers: drop -pie/-fPIC for gcc sanitizers (>=5.2)
    Since with the latest gcc we don't need this, and without this patch and latest
    linux kernel sanitizers fail with:
      FATAL: ThreadSanitizer: unexpected memory mapping 0x555caf24a000-0x555caf24b000
    
    Link: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67308
    
    P.S. Also drop OVERWRITE, since flags are the same.

--- Modules/SetupSanitizers.cmake
+++ Modules/SetupSanitizers.cmake
@@ -7,27 +7,34 @@ macro(SetupSanitizers)
     if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
         AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
                          FLAGS ${ARGV} -fsanitize=thread
-                         OVERWRITE -fsanitize=thread
                          LANGUAGES C CXX)
         AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
                          FLAGS "${ARGV} -fsanitize=address"
-                         OVERWRITE -fsanitize=address
                          LANGUAGES C CXX)
         AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEMEMORY
                          FLAGS ${ARGV} -fsanitize=memory -fno-omit-frame-pointer
                          OVERWRITE -fsanitize=memory -fsanitize-memory-track-origins
                          LANGUAGES C CXX)
     else()
-        AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
-                         FLAGS ${ARGV} "-fsanitize=thread -fPIC"
-                         OVERWRITE "-fsanitize=thread -pie"
-                         LANGUAGES C CXX)
-        set(CMAKE_EXE_LINKER_FLAGS_SANITIZETHREAD "-pie")
-        set(CMAKE_SHARED_LINKER_FLAGS_SANITIZETHREAD "-pie")
-
-        AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
-                         FLAGS ${ARGV} -fsanitize=address
-                         OVERWRITE -fsanitize=address
-                         LANGUAGES C CXX)
+        if ((CMAKE_COMPILER_IS_GNUCC) AND
+            (CMAKE_C_COMPILER_VERSION VERSION_GREATER "5.2"))
+            AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
+                             FLAGS ${ARGV} "-fsanitize=thread"
+                             LANGUAGES C CXX)
+            AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
+                             FLAGS ${ARGV} -fsanitize=address
+                             LANGUAGES C CXX)
+        else()
+            AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZETHREAD
+                             FLAGS ${ARGV} "-fsanitize=thread -fPIC"
+                             OVERWRITE "-fsanitize=thread -pie"
+                             LANGUAGES C CXX)
+            set(CMAKE_EXE_LINKER_FLAGS_SANITIZETHREAD "-pie")
+            set(CMAKE_SHARED_LINKER_FLAGS_SANITIZETHREAD "-pie")
+            AddCompilerFlags(FAIL_ON_ERROR BUILD_TYPE SANITIZEADDRESS
+                             FLAGS ${ARGV} -fsanitize=address
+                             OVERWRITE -fsanitize=address
+                             LANGUAGES C CXX)
+        endif()
     endif()
 endmacro()
